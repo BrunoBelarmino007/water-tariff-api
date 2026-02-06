9dcc5c14059ab6ab045736c553efa8d7d0ca2279
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2026-02-05 23:35:50 -0300

    chore: iniciando projeto Spring Boot com estrutura base e dependências essenciais

---
+++ src/main/java/com/gruporas/watertariffapi/WatertariffapiApplication.java
@@ -0,0 +1,13 @@
+package com.gruporas.watertariffapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatertariffapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatertariffapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gruporas/watertariffapi/WatertariffapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gruporas.watertariffapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatertariffapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
