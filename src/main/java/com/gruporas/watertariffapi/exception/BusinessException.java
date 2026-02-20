37cb7892e194cbd10b7fef6a71939d949493b534
Bruno Belarmino da Silva <brunobelarmino336@outlook.com>
2026-02-20 00:18:48 -0300

    feat(exception): adiciona BusinessException e ResourceNotFoundException

---
+++ src/main/java/com/gruporas/watertariffapi/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.gruporas.watertariffapi.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+    
+        super(message);
+    
+    }
+
+}
